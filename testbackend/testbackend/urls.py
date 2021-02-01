94bfedab4c5fd016a431045023f278c3393510fc
Miguel Villareal <57373312+miguelvillareal@users.noreply.github.com>
2021-02-01 12:15:23 -0700

    Update urls.py

--- testbackend/testbackend/urls.py
+++ testbackend/testbackend/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include(router.urls)),
+
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
