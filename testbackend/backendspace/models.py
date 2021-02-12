8d0a84ff92c61a60bdfc824d312e9e2527cf7f4d
Miguel Villareal <57373312+miguelvillareal@users.noreply.github.com>
2021-02-11 21:44:52 -0700

    Update models.py

--- testbackend/backendspace/models.py
+++ testbackend/backendspace/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.files import File
+from django.contrib.auth.models import User
+#from django.contrib.auth.models import PermissionsMixin
+#from django.contrib.auth.base_user import AbstractBaseUser
+#from django.contrib.gis.db import models
+#from django.contrib.gis.geos import Point
+from backendspace.UserManager import UserManager
+from django.contrib.auth.hashers import get_hasher, identify_hasher
+import uuid
 
 # Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True,db_index=True)
+    firstName = models.CharField(max_length=30, null=True, blank=True)
+    lastName = models.CharField(max_length=30, null=True, blank=True)
+    username = models.CharField(max_length=100, null=True, blank=True)
+    acceptPush = models.BooleanField(default=False)
+    pushToken = models.CharField(max_length=100, null=True, blank=True,db_index=True)
+    is_active = models.BooleanField(('active'), default=True)
+    valid = models.BooleanField(default=True)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    class Meta:
+        verbose_name = ('User')
+        verbose_name_plural = ('Users')
