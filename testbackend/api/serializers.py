70007e81153b09b40da5696bb5b880a8e0465e43
Miguel Villareal <57373312+miguelvillareal@users.noreply.github.com>
2021-02-24 22:40:02 -0700

    Update serializers.py

--- testbackend/api/serializers.py
+++ testbackend/api/serializers.py
@@ -4,3 +4,18 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class ExperimentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Experiment
+        fields = '__all__'
+
+class ProtocolsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Protocols
+        fields = '__all__'
+
+class LabGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LabGroup
+        fields = '__all__'
