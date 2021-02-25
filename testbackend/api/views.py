71d91a84c498bf834b472925f929d225d1e660a6
Miguel Villareal <57373312+miguelvillareal@users.noreply.github.com>
2021-02-24 22:40:59 -0700

    Update views.py

--- testbackend/api/views.py
+++ testbackend/api/views.py
@@ -1,5 +1,6 @@
 # *coding: utf-8*
 from backendspace.models import User
+from backendspace.models import Experiment
 from api.serializers import *
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
@@ -29,3 +30,34 @@ class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = User.objects.get_queryset().order_by('id')
     serializer_class = UserSerializer
+
+
+class ExperimentListCreateView(generics.ListCreateAPIView):
+    """
+            create:
+                add Experiments
+            get:
+                Search or get Experiments
+    """
+    queryset = Experiment.objects.get_queryset().order_by('step_num')
+    serializer_class = ExperimentSerializer
+
+class ProtocolsListCreateView(generics.ListCreateAPIView):
+    """
+            create:
+                add protocols
+            get:
+                Search or get protocols
+    """
+    queryset = Protocols.objects.get_queryset().order_by('name')
+    serializer_class = ProtocolsSerializer
+
+class LabGroupListCreateView(generics.ListCreateAPIView):
+    """
+            create:
+                add Lab Group
+            get:
+                Search or get LabGroups
+    """
+    queryset = LabGroup.objects.get_queryset().order_by('name')
+    serializer_class = LabGroupSerializer
