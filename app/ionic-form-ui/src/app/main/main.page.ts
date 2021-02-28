eced6d62f80a71e2892a7a277de694419da9ec03
Miguel Villareal <57373312+miguelvillareal@users.noreply.github.com>
2021-02-27 22:45:25 -0800

    Add files via upload

--- app/ionic-form-ui/src/app/main/main.page.ts
+++ app/ionic-form-ui/src/app/main/main.page.ts
@@ -1,6 +1,7 @@
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-main',
@@ -9,7 +10,11 @@ import { Router } from '@angular/router';
 })
 export class MainPage implements OnInit {
 
-  constructor(private authService: AuthenticationService, private router: Router) {}
+  constructor(private authService: AuthenticationService, private router: Router, private inAppBrowser: InAppBrowser) {}
+
+  redirect() {
+    this.inAppBrowser.create("http://127.0.0.1:8001/polls/");
+   }
  
   async logout() {
     await this.authService.logout();
