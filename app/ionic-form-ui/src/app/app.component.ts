125a88802ac6ca736bc253365d56e357396b85ec
Miguel Villareal <57373312+miguelvillareal@users.noreply.github.com>
2021-04-08 19:58:10 -0700

    Add files via upload

--- app/ionic-form-ui/src/app/app.component.ts
+++ app/ionic-form-ui/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { AuthenticationService } from 'src/app/services/authentication.service';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import {ApiDjangoService} from '../app/services/api-django.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,18 @@ import {ApiDjangoService} from '../app/services/api-django.service';
 export class AppComponent {
   constructor(
     private platform: Platform,
+    private authService: AuthenticationService,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    public apiService : ApiDjangoService
+    public apiService : ApiDjangoService,
+    private router: Router
   ) {
     this.initializeApp();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/home', { replaceUrl: true });
   }
 
   accessAuthorizedWithUrl(){
