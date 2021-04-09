2f038b9ac6a150c71f88a93be8dd0a68a8cf96d2
Miguel Villareal <57373312+miguelvillareal@users.noreply.github.com>
2021-04-08 20:02:33 -0700

    Add files via upload

--- app/ionic-form-ui/src/app/main/main.page.ts
+++ app/ionic-form-ui/src/app/main/main.page.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.page.html',
@@ -12,10 +13,6 @@ export class MainPage implements OnInit {
 
   constructor(private authService: AuthenticationService, private router: Router) {}
 
-  async logout() {
-    await this.authService.logout();
-    this.router.navigateByUrl('/', { replaceUrl: true });
-  }
   ngOnInit() {
   }
 
