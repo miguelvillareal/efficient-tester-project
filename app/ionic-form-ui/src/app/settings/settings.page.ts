dea9652e67ffbd32a4ae9a73f24c5d029c4e3837
Miguel Villareal <57373312+miguelvillareal@users.noreply.github.com>
2021-04-28 09:58:28 -0700

    Add files via upload

--- app/ionic-form-ui/src/app/settings/settings.page.ts
+++ app/ionic-form-ui/src/app/settings/settings.page.ts
@@ -5,6 +5,8 @@ import {myUserName} from 'src/app/services/authentication.service';
 import {myEmail} from 'src/app/services/authentication.service';
 import {myFirstName} from 'src/app/services/authentication.service';
 import {myLastName} from 'src/app/services/authentication.service';
+import {myID} from 'src/app/services/authentication.service';
+import { ApiDjangoService } from '../services/api-django.service';
 
 
 @Component({
@@ -12,13 +14,15 @@ import {myLastName} from 'src/app/services/authentication.service';
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
 })
+
 export class SettingsPage implements OnInit {
+  userCredentials = { email: '',};
   public userName : string = null;
   public userEmail : string = null;
   public userFirstName : string = null;
   public userLastName : string = null;
 
-  constructor() {
+  constructor(public apiService: ApiDjangoService) {
     this.userName = myUserName;
     this.userEmail = myEmail;
     this.userFirstName = myFirstName;
@@ -27,6 +31,18 @@ export class SettingsPage implements OnInit {
 
   ngOnInit() {
     //console.log(myID);
+  }
+
+  updateEmail(){
+    let queryPath = myID;
+    let setUserInfo = {
+      "email": this.userCredentials.email
+    }
+    this.apiService.infoUser(queryPath, setUserInfo).subscribe((myUser) => { 
+      if(myUser){
+        console.log(myUser);
+      }
+    });
   }
 
 }
