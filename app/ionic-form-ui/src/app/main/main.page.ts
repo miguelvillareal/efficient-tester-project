836c6eced6bfdf841cd01015893b0bf2bb6def32
Miguel Villareal <57373312+miguelvillareal@users.noreply.github.com>
2021-04-28 09:56:00 -0700

    Add files via upload

--- app/ionic-form-ui/src/app/main/main.page.ts
+++ app/ionic-form-ui/src/app/main/main.page.ts
@@ -1,6 +1,8 @@
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 
 
@@ -10,8 +12,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./main.page.scss'],
 })
 export class MainPage implements OnInit {
+  vid = 'https://youtube.com/embed/1-VAhbOwsOY';
+  constructor(private authService: AuthenticationService, private router: Router,
+    private dom: DomSanitizer, private inAppBrowser: InAppBrowser) {}
 
-  constructor(private authService: AuthenticationService, private router: Router) {}
+  sanitize(vid){
+    return this.dom.bypassSecurityTrustResourceUrl(vid);
+  }
+
+  redirect(){
+    this.router.navigateByUrl("/lab-group");
+  }
+
+  redirectTwo(){
+    this.router.navigateByUrl("/protocols");
+  }
+
+  redirectThree(){
+    this.inAppBrowser.create("https://ceias.nau.edu/capstone/projects/CS/2021/EfficientTest-F20/");
+  }
 
   ngOnInit() {
   }
